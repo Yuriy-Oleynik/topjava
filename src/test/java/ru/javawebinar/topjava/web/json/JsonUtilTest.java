fcca91159473fba3721e3daf2c56065263f7aa22
Юрий <yurikua11@gmail.com>
2018-09-28 09:15:48 +0300

    all patch 10.0 lesson

--- src/test/java/ru/javawebinar/topjava/web/json/JsonUtilTest.java
+++ src/test/java/ru/javawebinar/topjava/web/json/JsonUtilTest.java
@@ -1,10 +1,15 @@
 package ru.javawebinar.topjava.web.json;
 
+import com.fasterxml.jackson.databind.ObjectWriter;
 import org.junit.Test;
+import ru.javawebinar.topjava.View;
 import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.User;
 
 import java.util.List;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
 import static ru.javawebinar.topjava.MealTestData.*;
 
 public class JsonUtilTest {
@@ -23,5 +28,32 @@ public class JsonUtilTest {
         System.out.println(json);
         List<Meal> meals = JsonUtil.readValues(json, Meal.class);
         assertMatch(meals, MEALS);
+    }
+
+    @Test
+    public void testWriteWithView() throws Exception {
+        ObjectWriter uiWriter = JacksonObjectMapper.getMapper().writerWithView(View.JsonUI.class);
+        String json = JsonUtil.writeValue(ADMIN_MEAL1, uiWriter);
+        System.out.println(json);
+        assertThat(json, containsString("dateTimeUI"));
+    }
+
+    @Test
+    public void testWriteOnlyAccess() throws Exception {
+        String json = JsonUtil.writeValue(UserTestData.USER);
+        System.out.println(json);
+        assertThat(json, not(containsString("password")));
+        String jsonWithPass = UserTestData.jsonWithPassword(UserTestData.USER, "newPass");
+        System.out.println(jsonWithPass);
+        User user = JsonUtil.readValue(jsonWithPass, User.class);
+        Assert.assertEquals(user.getPassword(), "newPass");
+    }
+
+    @Test
+    public void testWriteWithView() throws Exception {
+        ObjectWriter uiWriter = JacksonObjectMapper.getMapper().writerWithView(View.JsonUI.class);
+        String json = JsonUtil.writeValue(ADMIN_MEAL1, uiWriter);
+        System.out.println(json);
+        assertThat(json, containsString("dateTimeUI"));
     }
 }
